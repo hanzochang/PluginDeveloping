41069d8ad02ae8fdbaac94c9a2e4f9fc6322b793
hanzochang <hanzo.chan@gmail.com>
2017-03-19 13:52:42 +0900

    Initialize Enviroments for Plugin Developing

---
+++ Source/PluginDeveloping/PluginDeveloping.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PluginDeveloping.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginDeveloping, "PluginDeveloping" );
---
+++ Source/PluginDeveloping/PluginDevelopingGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PluginDevelopingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINDEVELOPING_API APluginDevelopingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
